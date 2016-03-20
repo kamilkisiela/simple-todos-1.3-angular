bfaee16a8f129c87b4b10f74df8a6ba82a64ee22
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-03-20 13:16:11 +0100

    feat: step 5

--- client/main.js
+++ client/main.js
@@ -3,7 +3,17 @@ import angularMeteor from 'angular-meteor';
 
 import todosList from '../imports/components/todosList/todosList';
 
-angular.module('simple-todos',[
+angular.module('simple-todos', [
   angularMeteor,
   todosList.name
 ]);
+
+function onReady() {
+  angular.bootstrap(document, ['simple-todos']);
+}
+
+if (Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+} else {
+  angular.element(document).ready(onReady);
+}
